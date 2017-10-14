46e4313b96e53da41191d78624a0cde125da9ff5
Daniel Marshall <dmarshall2@sheffield.ac.uk>
2017-10-14 15:55:02 +0100

    Random quote works

--- api.rb
+++ api.rb
@@ -1,3 +1,5 @@
+require_relative 'skytest'
+
 require 'httparty'
 
 url = "http://partners.api.skyscanner.net/apiservices/geo/v1.0?apiKey=ha906464854775459164611892547937"
@@ -12,7 +14,7 @@ def randomcountry data
    continents.each do |j|
       countries = j['Countries']
      countries.each do |i|
-        countrylist.push(i['Name'])
+        countrylist.push(i['Id'])
      end
    end
 
@@ -26,8 +28,28 @@ def randomcountry data
    #end
 end
 
-def gethotel country checkindate checkoutdate minprice maxprice
+def gethotel(country, checkindate, checkoutdate, minprice, maxprice)
    
-   hotelurl = "https://gateway.skyscanner.net/hotels/v1/prices/search/entity/#{country}?market=GB&locale=en_GB&checkin_date=#{checkindate}&checkout_date=#{checkoutdate}&currency=GBP&adults=1&rooms=1&price_min=#{minprice}&price_max=#{maxprice}"
+  # hotelurl = "https://gateway.skyscanner.net/hotels/v1/prices/search/entity/#{country}?market=GB&locale=en_GB&checkin_date=#{checkindate}&checkout_date=#{checkoutdate}&currency=GBP&adults=1&rooms=1&price_min=#{minprice}&price_max=#{maxprice}&apikey=ha906464854775459164611892547937"
+
+  # hotelresponse = HTTParty.get(hotelurl, format: :json)
+  # hotelresponse.parsed_response
+
+  # return hotelresponse 
+end
+
+def pickquote(country, startdate, enddate)
+   quotesdata = findquote(country, startdate, enddate)
+   
+   quotes = quotesdata['Quotes']
+   quote = quotes.first
+   puts quote
+end
+
+choice = randomcountry(response)
+puts choice
+pickquote(choice, "2017-11-01", "2017-11-04")
+
+
 
       
--- skytest.rb
+++ skytest.rb
@@ -4,9 +4,10 @@ require 'geocoder'
 require 'rack'
 require 'httparty'
 
-def findquote(startdate, enddate)
-  url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/GBP/gb-GB/GB/anywhere/#{startdate}/#{enddate}?apiKey=ha906464854775459164611892547937"
+def findquote(country, startdate, enddate)
+  url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/GBP/gb-GB/GB/#{country}/#{startdate}/#{enddate}?apiKey=ha906464854775459164611892547937"
   response = HTTParty.get(url)
-  puts(response.parsed_response)
+  #puts(response.parsed_response)
+  return response.parsed_response
 end
 
