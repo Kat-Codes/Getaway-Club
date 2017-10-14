d10e4472ffd67fc8f72cafadc28e207b0f5f14af
Alex Chapman <achapman135@gmail.com>
2017-10-14 15:05:41 +0100

    made changes

--- main.rb
+++ main.rb
@@ -6,7 +6,13 @@ get '/' do
   erb :index
 end
 
-post '/' do
-    
-    erb :holiday
+
+post '/getholiday' do 
+  datein = swapdate(params[:datepicker])
+  dateout = swapdate(params[:datepicker2])
+end
+
+def swapdate(dateIn)
+  dateArr = dateIn.split('/')
+  return "#{dateArr[2]}-#{dateArr[0]}-#{dateArr[1]}"
 end
